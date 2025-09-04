978600f289b621fccde9c15cd9a2950995c4246a
Pratheep <ctgpratheep@gmail.com>
2025-09-04 10:46:40 +0530

    logo change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css"; 
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1 className="logo">Tutorial App</h1>
+      <div className="navbar-logo">
+        <img src="/logo LT.png" alt="LearnTube Logo" className="logo-img" />
+        <h1 className="logo-text">LearnTube</h1>
+      </div>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/category/IT">IT</Link>
         <Link to="/category/Mechanical">Mechanical</Link>
-        <Link to="/category/Civil">Civil</Link>
       </div>
     </nav>
   );
